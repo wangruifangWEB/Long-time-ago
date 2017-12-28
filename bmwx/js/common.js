85d197824bdd326126b6bbef49156530021396e2
wangruifang <wangruifang@buqiuren.com.cn>
2017-12-28 17:47:41 +0800

    同步代码

--- bmwx/js/common.js
+++ bmwx/js/common.js
@@ -5,3 +5,30 @@ $(function() {
 	var ws=(w*4/75).toFixed(5)+"px";
 	$("html").css({"font-size":ws});
 });
+
+//toggle 兼容处理
+$.fn.toggle = function( fn ) {
+    // Save reference to arguments for access in closure
+    var args = arguments,
+            guid = fn.guid || jQuery.guid++,
+            i = 0,
+            toggler = function( event ) {
+                // Figure out which function to execute
+                var lastToggle = ( jQuery._data( this, "lastToggle" + fn.guid ) || 0 ) % i;
+                jQuery._data( this, "lastToggle" + fn.guid, lastToggle + 1 );
+
+                // Make sure that clicks stop
+                event.preventDefault();
+
+                // and execute the function
+                return args[ lastToggle ].apply( this, arguments ) || false;
+            };
+
+        // link all the functions, so any of them can unbind this click handler
+    toggler.guid = guid;
+    while ( i < args.length ) {
+        args[ i++ ].guid = guid;
+    }
+
+    return this.click( toggler );
+}
